e47c4c5afb28d443841335c37aecc4fe5cb72b7c
ansango <anibalsantosgo@gmail.com>
2023-03-07 22:12:19 +0100

    feat(docs): user docs generated

--- src/api/user.ts
+++ src/api/user.ts
@@ -30,37 +30,126 @@ import type {
 import { buildUrl, fetcher } from "../utils";
 
 export type UserApiMethods = {
+  /**
+   * Fetches a list of the user's friends
+   * @param {UserGetFriendsRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetFriendsResponse>}
+   * https://www.last.fm/api/show/user.getFriends
+   */
   getFriends: (
-    params: UserGetFriendsRequest
+    params: UserGetFriendsRequest,
+    init?: RequestInit
   ) => Promise<UserGetFriendsResponse>;
-  getInfo: (params: UserGetInfoRequest) => Promise<UserGetInfoResponse>;
+  /**
+   * Fetch user info
+   * @param {UserGetInfoRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetInfoResponse>}
+   * https://www.last.fm/api/show/user.getInfo
+   */
+
+  getInfo: (
+    params: UserGetInfoRequest,
+    init?: RequestInit
+  ) => Promise<UserGetInfoResponse>;
+  /**
+   * Fetches a list of the user's loved tracks
+   * @param {UserGetLovedTracksRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetLovedTracksResponse>}
+   * https://www.last.fm/api/show/user.getLovedTracks
+   */
   getLovedTracks: (
-    params: UserGetLovedTracksRequest
+    params: UserGetLovedTracksRequest,
+    init?: RequestInit
   ) => Promise<UserGetLovedTracksResponse>;
+  /**
+   * Fetches a list of the user's recent tracks
+   * @param {UserGetRecentTracksRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetRecentTracksResponse>}
+   * https://www.last.fm/api/show/user.getRecentTracks
+   */
   getRecentTracks: (
     params: UserGetRecentTracksRequest
   ) => Promise<UserGetRecentTracksResponse>;
+  /**
+   * Fetches a list of the user's top albums
+   * @param {UserGetTopAlbumsRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetTopAlbumsResponse>}
+   * https://www.last.fm/api/show/user.getTopAlbums
+   */
   getTopAlbums: (
     params: UserGetTopAlbumsRequest
   ) => Promise<UserGetTopAlbumsResponse>;
+  /**
+   * Fetches a list of the user's top artists
+   * @param {UserGetTopArtistsRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetTopArtistsResponse>}
+   * https://www.last.fm/api/show/user.getTopArtists
+   */
   getTopArtists: (
     params: UserGetTopArtistsRequest
   ) => Promise<UserGetTopArtistsResponse>;
+  /**
+   * Fetches a list of the user's top tags
+   * @param {UserGetTopTagsRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetTopTagsResponse>}
+   * https://www.last.fm/api/show/user.getTopTags
+   */
   getTopTags: (
     params: UserGetTopTagsRequest
   ) => Promise<UserGetTopTagsResponse>;
+  /**
+   * Fetches a list of the user's top tracks
+   * @param {UserGetTopTracksRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetTopTracksResponse>}
+   * https://www.last.fm/api/show/user.getTopTracks
+   */
   getTopTracks: (
     params: UserGetTopTracksRequest
   ) => Promise<UserGetTopTracksResponse>;
+  /**
+   * Fetches a list of the user's chart albums for a given week
+   * @param {UserGetWeeklyAlbumChartRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetWeeklyAlbumChartResponse>}
+   * https://www.last.fm/api/show/user.getWeeklyAlbumChart
+   */
   getWeeklyAlbumChart: (
     params: UserGetWeeklyAlbumChartRequest
   ) => Promise<UserGetWeeklyAlbumChartResponse>;
+  /**
+   * Fetches a list of the user's chart artists for a given week
+   * @param {UserGetWeeklyArtistChartRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetWeeklyArtistChartResponse>}
+   * https://www.last.fm/api/show/user.getWeeklyArtistChart
+   */
   getWeeklyArtistChart: (
     params: UserGetWeeklyArtistChartRequest
   ) => Promise<UserGetWeeklyArtistChartResponse>;
+  /**
+   * Fetches a list of the user's chart lists for a given week
+   * @param {UserGetWeeklyChartListRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetWeeklyChartListResponse>}
+   * https://www.last.fm/api/show/user.getWeeklyChartList
+   */
   getWeeklyChartList: (
     params: UserGetWeeklyChartListRequest
   ) => Promise<UserGetWeeklyChartListResponse>;
+  /**
+   * Fetches a list of the user's chart tracks for a given week
+   * @param {UserGetWeeklyTrackChartRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<UserGetWeeklyTrackChartResponse>}
+   */
   getWeeklyTrackChart: (
     params: UserGetWeeklyTrackChartRequest
   ) => Promise<UserGetWeeklyTrackChartResponse>;
@@ -72,11 +161,13 @@ export const userApiMethods: UserApiMethods = {
       buildUrl(method.user.getFriends, { ...params }),
       init
     ),
+
   getInfo: (params: UserGetInfoRequest, init?: RequestInit) =>
     fetcher<UserGetInfoResponse>(
       buildUrl(method.user.getInfo, { ...params }),
       init
     ),
+
   getLovedTracks: (params: UserGetLovedTracksRequest, init?: RequestInit) =>
     fetcher<UserGetLovedTracksResponse>(
       buildUrl(method.user.getLovedTracks, { ...params }),
