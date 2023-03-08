8d3f612724bb45690bd6cbf244370958349bf07e
ansango <anibalsantosgo@gmail.com>
2023-03-08 02:50:25 +0100

    feat(chart): service and types

--- dist/api/chart.d.ts
+++ dist/api/chart.d.ts
@@ -1,2 +1,28 @@
-export type ChartApiMethods = {};
+import type { ChartGetTopArtistsRequest, ChartGetTopArtistsResponse, ChartGetTopTagsRequest, ChartGetTopTagsResponse, ChartGetTopTracksRequest, ChartGetTopTracksResponse } from "../types/chart";
+export type ChartApiMethods = {
+    /**
+     * Get the top artists chart
+     * @param {ChartGetTopArtistsRequest} params
+     * @param {RequestInit} init
+     * @returns {Promise<ChartGetTopArtistsResponse>}
+     * https://www.last.fm/api/show/chart.getTopArtists
+     * */
+    getTopArtists: (params: ChartGetTopArtistsRequest, init?: RequestInit) => Promise<ChartGetTopArtistsResponse>;
+    /**
+     * Get the top tags chart
+     * @param {ChartGetTopTagsRequest} params
+     * @param {RequestInit} init
+     * @returns {Promise<ChartGetTopTagsResponse>}
+     * https://www.last.fm/api/show/chart.getTopTags
+     * */
+    getTopTags: (params: ChartGetTopTagsRequest, init?: RequestInit) => Promise<ChartGetTopTagsResponse>;
+    /**
+     * Get the top tracks chart
+     * @param {ChartGetTopTracksRequest} params
+     * @param {RequestInit} init
+     * @returns {Promise<ChartGetTopTracksResponse>}
+     * https://www.last.fm/api/show/chart.getTopTracks
+     * */
+    getTopTracks: (params: ChartGetTopTracksRequest, init?: RequestInit) => Promise<ChartGetTopTracksResponse>;
+};
 export declare const chartApiMethods: ChartApiMethods;
--- dist/api/chart.js
+++ dist/api/chart.js
@@ -1,4 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.chartApiMethods = void 0;
-exports.chartApiMethods = {};
+const method_1 = require("../method");
+const utils_1 = require("../utils");
+exports.chartApiMethods = {
+    getTopArtists: (params, init) => (0, utils_1.fetcher)((0, utils_1.buildUrl)(method_1.method.chart.getTopArtists, params), init),
+    getTopTags: (params, init) => (0, utils_1.fetcher)((0, utils_1.buildUrl)(method_1.method.chart.getTopTags, params), init),
+    getTopTracks: (params, init) => (0, utils_1.fetcher)((0, utils_1.buildUrl)(method_1.method.chart.getTopTracks, params), init),
+};
