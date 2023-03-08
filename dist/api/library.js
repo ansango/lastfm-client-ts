1a895510e5125aa128f095f97d39a13bf20de0a6
ansango <anibalsantosgo@gmail.com>
2023-03-08 02:30:26 +0100

    feat(library): service and type library

--- dist/api/library.js
+++ dist/api/library.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.libraryApiMethods = void 0;
-exports.libraryApiMethods = {};
+const method_1 = require("../method");
+const utils_1 = require("../utils");
+exports.libraryApiMethods = {
+    getArtists: (params, init) => (0, utils_1.fetcher)((0, utils_1.buildUrl)(method_1.method.library.getArtists, params), init),
+};
--- src/api/library.ts
+++ src/api/library.ts
@@ -1,3 +1,29 @@
-export type LibraryApiMethods = {};
+import { method } from "../method";
+import type {
+  LibraryGetArtistsRequest,
+  LibraryGetArtistsResponse,
+} from "../types/library";
 
-export const libraryApiMethods: LibraryApiMethods = {};
+import { fetcher, buildUrl } from "../utils";
+
+export type LibraryApiMethods = {
+  /**
+   * Get a list of the artists saved in a user's Last.fm library, along with the number of plays by the user.
+   * @param {LibraryGetArtistsRequest} params
+   * @param {RequestInit} init
+   * @returns {Promise<LibraryGetArtistsResponse>}
+   * https://www.last.fm/api/show/library.getArtists
+   * */
+  getArtists: (
+    params: LibraryGetArtistsRequest,
+    init?: RequestInit
+  ) => Promise<LibraryGetArtistsResponse>;
+};
+
+export const libraryApiMethods: LibraryApiMethods = {
+  getArtists: (params, init) =>
+    fetcher<LibraryGetArtistsResponse>(
+      buildUrl(method.library.getArtists, params),
+      init
+    ),
+};
