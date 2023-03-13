c30c9d0bd9bf4d6a526dcee83681b3f2902d910d
ansango <anibalsantosgo@gmail.com>
2023-03-13 23:54:25 +0100

    perf(build): build client

--- dist/types/album.d.ts
+++ dist/types/album.d.ts
@@ -1,150 +1,118 @@
-import type {
-  AlbumName,
-  ArtistName,
-  Lang,
-  Mbid,
-  UserName,
-  TagName,
-  Url,
-  Count,
-  Limit,
-  Page,
-  Text,
-  TotalResults,
-  StartIndex,
-  ItemsPerPage,
-  For,
-  Image,
-  Duration,
-  Name,
-  Published,
-  Summary,
-  Content,
-  Playcount,
-  Listeners,
-  Role,
-  SearchTerms,
-  StartPage,
-} from "./base";
+import type { AlbumName, ArtistName, Lang, Mbid, UserName, TagName, Url, Count, Limit, Page, Text, TotalResults, StartIndex, ItemsPerPage, For, Image, Duration, Name, Published, Summary, Content, Playcount, Listeners, Role, SearchTerms, StartPage } from "./base";
 type AlbumTag = {
-  tag?:
-    | Array<{
+    tag: Array<{
         name: TagName;
         url: Url;
-      }>
-    | {
-        name: TagName;
-        url: Url;
-      };
+    }>;
 };
 type AlbumTrackArtist = {
-  name: ArtistName;
-  mbid: Mbid;
-  url: Url;
+    name: ArtistName;
+    mbid: Mbid;
+    url: Url;
 };
 type AlbumTrack = {
-  track: Array<{
-    duration: Duration;
-    name: Name;
-    url: Url;
-    artist: AlbumTrackArtist;
-  }>;
+    track: Array<{
+        duration: Duration;
+        name: Name;
+        url: Url;
+        artist: AlbumTrackArtist;
+    }>;
 };
 type AlbumWiki = {
-  published: Published;
-  summary: Summary;
-  content: Content;
+    published: Published;
+    summary: Summary;
+    content: Content;
 };
 /**
  * Album
  * @see https://www.last.fm/api/show/album.getInfo
  */
 export type Album = {
-  artist: ArtistName;
-  mbid: Mbid;
-  tags: AlbumTag;
-  playcount: Playcount;
-  image: Array<Image>;
-  tracks: AlbumTrack;
-  url: Url;
-  name: AlbumName;
-  listeners: Listeners;
-  wiki: AlbumWiki;
+    artist: ArtistName;
+    mbid: Mbid;
+    tags: AlbumTag;
+    playcount: Playcount;
+    image: Array<Image>;
+    tracks: AlbumTrack;
+    url: Url;
+    name: AlbumName;
+    listeners: Listeners;
+    wiki: AlbumWiki;
 };
 export type AlbumGetInfoRequest = {
-  artist: ArtistName;
-  album: AlbumName;
-  mbid?: Mbid;
-  username?: UserName;
-  lang?: Lang;
+    artist: ArtistName;
+    album: AlbumName;
+    mbid?: Mbid;
+    username?: UserName;
+    lang?: Lang;
 };
 export type AlbumGetInfoResponse = {
-  album: Album;
+    album: Album;
 };
 export type AlbumGetTagsRequest = {
-  artist: ArtistName;
-  album: AlbumName;
-  mbid?: Mbid;
-  user: UserName;
+    artist: ArtistName;
+    album: AlbumName;
+    mbid?: Mbid;
+    user: UserName;
 };
 export type AlbumGetTagsResponse = {
-  tags: {
-    tag: Array<{
-      name: TagName;
-      url: Url;
-    }>;
-  };
-  "@attr": {
-    album: AlbumName;
-    artist: ArtistName;
-  };
-};
-export type AlbumGetTopTagsRequest = {
-  artist: ArtistName;
-  album: AlbumName;
-  mbid?: Mbid;
-};
-export type AlbumGetTopTagsResponse = {
-  tags: {
-    tag: Array<{
-      name: TagName;
-      url: Url;
-      count: Count;
-    }>;
-  };
-  "@attr": {
-    album: AlbumName;
-    artist: ArtistName;
-  };
-};
-export type AlbumSearchRequest = {
-  album: AlbumName;
-  limit?: Limit;
-  page?: Page;
-};
-export type AlbumSearchResponse = {
-  results: {
-    "opensearch:Query": {
-      "#text": Text;
-      role: Role;
-      searchTerms: SearchTerms;
-      startPage: StartPage;
-    };
-    "opensearch:totalResults": TotalResults;
-    "opensearch:startIndex": StartIndex;
-    "opensearch:itemsPerPage": ItemsPerPage;
-    albummatches: {
-      album: Array<{
-        name: AlbumName;
-        artist: ArtistName;
-        url: Url;
-        image: Array<Image>;
-        mbid: Mbid;
-      }>;
+    tags: {
+        tag: Array<{
+            name: TagName;
+            url: Url;
+        }>;
     };
     "@attr": {
-      for: For;
+        album: AlbumName;
+        artist: ArtistName;
     };
-  };
+};
+export type AlbumGetTopTagsRequest = {
+    artist: ArtistName;
+    album: AlbumName;
+    mbid?: Mbid;
+};
+export type AlbumGetTopTagsResponse = {
+    tags: {
+        tag: Array<{
+            name: TagName;
+            url: Url;
+            count: Count;
+        }>;
+    };
+    "@attr": {
+        album: AlbumName;
+        artist: ArtistName;
+    };
+};
+export type AlbumSearchRequest = {
+    album: AlbumName;
+    limit?: Limit;
+    page?: Page;
+};
+export type AlbumSearchResponse = {
+    results: {
+        "opensearch:Query": {
+            "#text": Text;
+            role: Role;
+            searchTerms: SearchTerms;
+            startPage: StartPage;
+        };
+        "opensearch:totalResults": TotalResults;
+        "opensearch:startIndex": StartIndex;
+        "opensearch:itemsPerPage": ItemsPerPage;
+        albummatches: {
+            album: Array<{
+                name: AlbumName;
+                artist: ArtistName;
+                url: Url;
+                image: Array<Image>;
+                mbid: Mbid;
+            }>;
+        };
+        "@attr": {
+            for: For;
+        };
+    };
 };
 export {};
